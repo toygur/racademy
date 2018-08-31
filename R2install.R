3f27b253dbb8ae76877bab3bda31179cfd182045
toygur <tunc.oygur@gmail.com>
2018-08-31 22:25:37 +0300

    install

---
+++ R2install.R
@@ -0,0 +1,26 @@
+
+install.RMetrics <- function(){
+  
+  install.packages(c("devtools","fAsianOptions", "fAssets", "fBasics", "fBonds",
+                     "fCopulae", "fExoticOptions", "fExtremes", "fGarch", "fImport",
+                     "fNonlinear", "fOptions", "fPortfolio", "fRegression", "fTrading",
+                     "fUnitRoots"))
+  
+}
+
+
+install.tidyPack <- function(){
+  
+  install.packages(c("magrittr","lubridate",
+                     "tidyverse",
+                     "tidyquant","tibbletime","timetk",
+                     "readxl","haven","jsonlite","xml2","httr","rvest"))
+  
+}
+
+
+
+
+
+
+
